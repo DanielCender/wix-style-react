f21ce46460d7206a24b3db18dfeac785e7f7560a
TalZa <24567980+TalZa1810@users.noreply.github.com>
2020-04-06 19:59:43 +0300

    improving story

--- src/new-icons/docs/AllIcons.js
+++ src/new-icons/docs/AllIcons.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import * as Icons from 'wix-ui-icons-common';
+import s from './AllIcons.scss';
 
-const renderIcon = name => {
-  return (
-    <div>
-      <span>{React.createElement(Icons[name])}</span>
-      <span>{name}</span>
-    </div>
-  );
-};
+const renderIcon = name => (
+  <div className={s.singleIconView} key={name}>
+    <span>{React.createElement(Icons[name])}</span>
+    <span className={s.iconName}>{name}</span>
+  </div>
+);
 
-const AllIcons = () => <div>{Object.keys(Icons).map(renderIcon)}</div>;
+const AllIcons = () => (
+  <div className={s.iconList}>{Object.keys(Icons).map(renderIcon)}</div>
+);
 
 export default AllIcons;
--- src/new-icons/docs/index.story.js
+++ src/new-icons/docs/index.story.js
@@ -9,6 +9,9 @@ import {
   example as baseExample,
   api,
 } from 'wix-storybook-utils/Sections';
+import CodeExample from 'wix-storybook-utils/CodeExample';
+import AllIcons from './AllIcons';
+import AllIconsRaw from '!raw-loader!./AllIcons';
 
 import allComponents from '../../../stories/utils/allComponents';
 import { storySettings } from './storySettings';
@@ -43,11 +46,6 @@ export default {
           title('Examples'),
 
           example({
-            title: 'All Icons',
-            source: '<div />',
-          }),
-
-          example({
             title: 'Custom Icon color',
             description:
               'The icon color can be customized using the `style` prop or `className` prop. These are the recommended colors.',
@@ -69,6 +67,10 @@ export default {
             title: 'Usage',
             source: examples.usageExample,
           }),
+
+          <CodeExample title="All Icons" code={AllIconsRaw}>
+            <AllIcons />
+          </CodeExample>,
         ],
       }),
 
